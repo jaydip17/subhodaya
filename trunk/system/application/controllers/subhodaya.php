7090b59e7175af9acc5e8e4d0b0dc02dfc42cf30
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-07-06 12:09:17 +0000

    home page

--- trunk/system/application/controllers/subhodaya.php
+++ trunk/system/application/controllers/subhodaya.php
@@ -5,10 +5,13 @@ class Subhodaya extends Controller {
 		parent::Controller();
 		$this->load->helper('directory');
 		$this->load->model('admin/News_Model');
+		$this->load->model("admin/Cinema_Model");
 	}
 	function index(){
+		$cinema_type2=$this->Cinema_Model->get_cinematype(2);
 		$more=$this->News_Model->more_news();
-		$data=array('more'=>$more);
+		$data=array('more'=>$more,
+					'cinema_type2'=>$cinema_type2);
 		$this->load->view('home',$data);
 	}
 }

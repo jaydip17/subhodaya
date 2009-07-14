14c2d304477eb0b109a974b4e17af188f40a46e0
rampelli.pradeep <rampelli.pradeep@gmail.com>
2009-07-14 10:58:06 +0000

    yahoo contacts grabber

--- system/plugins/grabcontacts_pi.php
+++ system/plugins/grabcontacts_pi.php
@@ -29,7 +29,58 @@ $resultarray = get_contacts($login, $password);
         $formdisclaimer = "<br><b style=\"color:red\">Enter Your Username and Password</b><br>";
         break;
     }
-    echo "error";
+   redirect(base_url().'admin/contacts/index/1');
   	//include("form.php");
   }
+  
+  
 }
+
+//grab yahoo contacts
+  
+  function grabyahoo($login,$password)
+  {
+  	require 'baseclass/baseclass.php';
+  	require("yahoo/class.GrabYahoo.php");
+	 		$obj = new GrabYahoo();
+	 		$contacts = $obj->getAddressbook($login,$password);
+	 		$fp = fopen("./assets/cookie.txt","w+");
+		fwrite($fp,"");				
+		fclose($fp);
+	 	if(!is_array($contacts))
+	 	{
+	 		redirect(base_url().'admin/contacts/index/1');
+	 		//die('<p align="center"><font color="#FF0000">No contacts found</font></p>');
+	 	}
+		if(is_array($contacts))
+		{
+			$totalRecords=0;
+			$actualfile = $login.time().".csv";
+//        	$fileName="./assets/csvUpload/".$actualfile;
+//        
+//			$handler= fopen($fileName,"a");
+//			fwrite($handler,"NAME".","."EMAIL"."\n");
+		
+			$total = sizeof($contacts['name']);
+			
+			//print the addressbook 
+			
+			for ($i=0;$i< $total;$i++) 
+			{
+				$totalRecords = $totalRecords+1;
+				$rep 		  = array("<br>","&nbsp;");
+				
+				
+				$contacts['email'][$i] = str_replace($rep, "",$contacts['email'][$i]);
+				$contacts['name'][$i]  = str_replace($rep, "",$contacts['name'][$i]);
+				$names[$i] = $contacts['name'][$i];
+				$emails[$i] = $contacts['email'][$i];
+				//fwrite($handler,$contacts['name'][$i].",".$contacts['email'][$i]."\n");
+			}
+			
+			//fclose($handler);
+		}
+		$result = array($names,$emails);
+		return $result;
+		//return array($names,$emails);
+  }

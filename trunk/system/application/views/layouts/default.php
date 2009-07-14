c72dd7dfd88dccc07a4c24499b54b40702eeb8fb
srivalli.ou <srivalli.ou@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-07-14 09:29:47 +0000

    news page pagination added for different views

--- trunk/system/application/views/layouts/default.php
+++ trunk/system/application/views/layouts/default.php
@@ -14,52 +14,16 @@
 <!-- tabs -->
 <script src="<?=base_url();?>assets/SpryAssets/SpryTabbedPanels.js" type="text/javascript"></script>
 <link href="<?=base_url();?>assets/SpryAssets/SpryTabbedPanels.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript"><!--
-
-var xmlhttp;
-
-function loadPoll(url,somevalue)
-{
-
- url = url+'/'+somevalue;
-xmlhttp=null;
-if (window.XMLHttpRequest)
-  {// code for IE7, Firefox, Mozilla, etc.
-  xmlhttp=new XMLHttpRequest();
-  }
-else if (window.ActiveXObject)
-  {// code for IE5, IE6
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-if (xmlhttp!=null)
-  {
-  xmlhttp.onreadystatechange=get_Poll;
-  xmlhttp.open("GET",url,true);
-  xmlhttp.send(null);
-  }
-else
-  {
-  alert("Your browser does not support XMLHTTP.");
-  }
-}
-function get_Poll()
-{
-if (xmlhttp.readyState==4)
-  {// 4 = "loaded"
-  if (xmlhttp.status==200)
-    {// 200 = "OK"
-    document.getElementById('home_poll').innerHTML=xmlhttp.responseText;
-    }
-  else
-    {
-    alert("Problem retrieving XML data:" + xmlhttp.statusText);
-    }
-  }
-}
-
---></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/pollLoader.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/newsLoader.js"></script>
 </head>
-<body >
+<?if($this->uri->segment(2)=='details'){;
+$type=$this->uri->segment(3);
+?>
+<body onload="loadNews('<?=base_url()?>newslist/listview/<?=$type?>')">
+<?}else{ ?>
+<body>
+<?} ?>
 
 <table width="999" height="1330"  border="0" align="center" cellpadding="0" cellspacing="0" >
   <tr>
--- trunk/system/application/views/linklistview.php
+++ trunk/system/application/views/linklistview.php
@@ -2,6 +2,6 @@
 <ul id="mainnews">
 <li><a href="<?=base_url()?>news/newsdeatils/<?=$row->id?>"><? echo $row->heading; ?></a></li>
 </ul>
-<? endforeach; ?>
+<?   endforeach;  echo $paginate; ?>
 
 

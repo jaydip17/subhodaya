6e8fa334358fd9e22db2b12d43a9819d58f19f29
rampelli.pradeep <rampelli.pradeep@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-08-20 09:46:26 +0000

    flash added

---
+++ system/application/controllers/flash.php
@@ -0,0 +1,27 @@
+<?php
+class Flash extends Controller {
+	function Flash()
+	{
+		parent::Controller();
+	}
+function flashimages()
+	{
+echo "<images>
+<preview1>images/flash-im1.jpg</preview1>
+<preview2>images/flash-im2.jpg</preview2>
+<preview3>images/flash-im3.jpg</preview3>
+<preview4>images/flash-im4.jpg</preview4>
+<preview5>images/flash-im5.jpg</preview5>
+<thumb1>images/1.jpg</thumb1>
+<thumb2>images/2.jpg</thumb2>
+<thumb3>images/3.jpg</thumb3>
+<thumb4>images/4.jpg</thumb4>
+<thumb5>images/5.jpg</thumb5>
+<content1>content1</content1>
+<content2>content2</content2>
+<content3>content3</content3>
+<content4>content4</content4>
+<content5>content5</content5>
+</images>";
+	}
+}

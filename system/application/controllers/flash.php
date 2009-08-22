d8322c5de3a9871b2d2ee0a89e0241370a574afa
srivalli.ou <srivalli.ou@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-08-22 14:09:01 +0000

    cinema gallery admin and main from database

--- system/application/controllers/flash.php
+++ system/application/controllers/flash.php
@@ -3,25 +3,22 @@ class Flash extends Controller {
 	function Flash()
 	{
 		parent::Controller();
+		$this->load->model('admin/Flash_model');
 	}
 function flashimages()
-	{
-echo "<images>
-<preview1>images/flash-im1.jpg</preview1>
-<preview2>images/flash-im2.jpg</preview2>
-<preview3>images/flash-im3.jpg</preview3>
-<preview4>images/flash-im4.jpg</preview4>
-<preview5>images/flash-im5.jpg</preview5>
-<thumb1>images/1.jpg</thumb1>
-<thumb2>images/2.jpg</thumb2>
-<thumb3>images/3.jpg</thumb3>
-<thumb4>images/4.jpg</thumb4>
-<thumb5>images/5.jpg</thumb5>
-<content1>content1 dsf sdf dsfdsf dsf dsfdsf dsf dsfds f dsf ds</content1>
-<content2>content2f dsf dsf dsdsf dsf sdfdsf dsf dsf dfsdf dfsdfdsfdsf fdg fd gfdg fdg fdg fdg fdg fdg fdg fdg fdg fd gfg dfg df</content2>
-<content3>content3</content3>
-<content4>content4</content4>
-<content5>content5</content5>
-</images>";
-	}
+{
+	$query=$this->Flash_model->get_flash_images();
+	$images=$query->result();
+    $output="<images>";
+    		$i=1;
+    		foreach($images as $item)
+		{
+		  $output.="<preview".$i.">flash_images/image".$item->id.".jpg</preview".$i.">";
+		  $output.="<thumb".$i.">flash_images/image".$item->id."_thumb.jpg</thumb".$i.">";
+		  $output.="<content".$i.">".$item->title."</content".$i.">";
+		  $output.="<hyperlink".$i.">".$item->hyperlink."</hyperlink".$i.">";
+		  $i++;		}
+		$output.="</images>";
+		print($output);
+		exit;	}
 }

a8fdbd6de4547f106d4690daabc260d626378fbc
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-08-24 10:35:20 +0000

    home styles modification

--- trunk/system/application/controllers/news.php
+++ trunk/system/application/controllers/news.php
@@ -18,16 +18,19 @@ class News extends Controller {
 		$news_type7=$this->News_Model->get_newstype1(7);
 		$newspoll=$this->Poll_Model->get_newspolls($type=4);
 		$yes_poll=$this->Poll_Model->get_yes_newspoll($type=4);
-		$data=array('news_type1'=>$details,
-					'news_type2'=>$news_type2,
-					'news_type3'=>$news_type3,
-					'news_type4'=>$news_type4,
-					'news_type5'=>$news_type5,
-					'news_type6'=>$news_type6,
-					'news_type7'=>$news_type7,
-					'more'      =>$more,
-					'newspoll'=>$newspoll,
-					'yes_poll'=>$yes_poll
+		$active_news1=$this->News_Model->active_news(1);
+		//print_r($active_news1);
+		$data=array('news_type1'  =>  $details,
+					'news_type2'  =>  $news_type2,
+					'news_type3'  =>  $news_type3,
+					'news_type4'  =>  $news_type4,
+					'news_type5'  =>  $news_type5,
+					'news_type6'  =>  $news_type6,
+					'news_type7'  =>  $news_type7,
+					'more'        =>  $more,
+					'newspoll'    =>  $newspoll,
+					'yes_poll'    =>  $yes_poll,
+					'active_news1'=>  $active_news1
 		);
 		$this->load->view("news_main",$data);
 	}
@@ -37,12 +40,6 @@ class News extends Controller {
 		$more=$this->News_Model->more_news();
 		$type=$this->uri->segment(3,0);
 		$totel_rows=$this->News_Model->get_news($type,$count=true,$limit=0);
-	/*	$this->load->library('pagination');
-		$config['base_url'] = base_url()."news/details/";
-		$config['total_rows'] = $totel_rows;
-		$config['per_page'] = '13';
-		$this->pagination->initialize($config); 
-		$pagination=$this->pagination->create_links();*/
 		$news=$this->News_Model->get_news($type,$count=false);
 		$data=array(	'news'  =>$news,
 						'more'=>$more,

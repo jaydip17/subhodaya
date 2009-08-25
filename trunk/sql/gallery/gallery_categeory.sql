9a51a58dd9822053aefac9d826a1241074f9e457
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-08-25 10:43:13 +0000

    gallery tables

--- trunk/sql/gallery/gallery_categeory.sql
+++ trunk/sql/gallery/gallery_categeory.sql
@@ -1,13 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.5
+-- version 3.1.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 21, 2009 at 09:02 AM
--- Server version: 5.0.51
--- PHP Version: 5.2.5
+-- Generation Time: Aug 25, 2009 at 10:39 AM
+-- Server version: 5.1.32
+-- PHP Version: 5.2.9-1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `subhodaya`
@@ -21,10 +27,30 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 DROP TABLE IF EXISTS `gallery_categeory`;
 CREATE TABLE IF NOT EXISTS `gallery_categeory` (
-  `id` int(20) NOT NULL auto_increment,
-  `catname` varchar(200) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `catname` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `parentid` int(20) NOT NULL,
   `active` int(10) NOT NULL,
-  `update_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+
+--
+-- Dumping data for table `gallery_categeory`
+--
+
+INSERT INTO `gallery_categeory` (`id`, `catname`, `parentid`, `active`, `update_date`) VALUES
+(11, 'తారామణులు', 0, 0, '2009-08-22 10:46:29'),
+(12, 'దేవుళ్ళు', 0, 0, '2009-08-22 10:47:05'),
+(13, 'సినిమా', 0, 0, '2009-08-22 10:47:20'),
+(14, 'అనుష్క', 11, 0, '2009-08-22 10:55:41'),
+(15, 'సాయి బాబా', 12, 0, '2009-08-22 11:01:05'),
+(16, 'వినాయకుడు', 12, 0, '2009-08-22 11:01:49'),
+(17, 'ప్రస్థానం', 13, 0, '2009-08-22 11:02:11'),
+(18, 'శ్రియ', 11, 0, '2009-08-25 14:36:37'),
+(19, 'జెనిలియా', 11, 0, '2009-08-22 11:53:23'),
+(20, 'ఫోటో గ్యాలరీ', 0, 0, '2009-08-25 13:49:14'),
+(21, 'పువ్వులు', 20, 0, '2009-08-25 13:50:22'),
+(22, 'హీరోస్', 20, 0, '2009-08-25 14:36:46'),
+(23, 'హీరోయిన్స్', 20, 0, '2009-08-25 14:37:20'),
+(24, 'కమిడియన్స్', 20, 0, '2009-08-25 14:34:02');
--- trunk/sql/gallery/gallery_images.sql
+++ trunk/sql/gallery/gallery_images.sql
@@ -1,13 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.5
+-- version 3.1.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 21, 2009 at 09:02 AM
--- Server version: 5.0.51
--- PHP Version: 5.2.5
+-- Generation Time: Aug 25, 2009 at 10:40 AM
+-- Server version: 5.1.32
+-- PHP Version: 5.2.9-1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `subhodaya`
@@ -21,9 +27,39 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 DROP TABLE IF EXISTS `gallery_images`;
 CREATE TABLE IF NOT EXISTS `gallery_images` (
-  `id` int(20) NOT NULL auto_increment,
-  `title` varchar(200) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `parentid` int(20) NOT NULL,
   `active` int(10) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+--
+-- Dumping data for table `gallery_images`
+--
+
+INSERT INTO `gallery_images` (`id`, `title`, `parentid`, `active`) VALUES
+(4, 'అనుష్క-రవితేజ ', 14, 0),
+(5, 'షిరిడి సాయి బాబా', 15, 0),
+(6, 'అనుష్క&nbsp; <br>', 14, 0),
+(7, 'ప్రస్తానం <br>', 17, 0),
+(8, 'శ్రియ&nbsp; <br>', 18, 0),
+(9, 'ప్రస్తానం <br>', 17, 0),
+(10, 'సాయి <br>', 15, 0),
+(11, 'జెనిలియా', 19, 0),
+(12, 'జెనిలియా', 19, 0),
+(13, 'జెనిలియా', 19, 0),
+(14, 'ప్రస్థానం', 17, 0),
+(15, 'ప్రస్థానం', 17, 0),
+(16, 'ప్రస్థానం', 17, 0),
+(17, 'ప్రస్థానం', 17, 0),
+(18, 'ప్రస్థానం', 17, 0),
+(19, 'ప్రస్థానం', 17, 0),
+(20, 'రామ్ చరణ్ తెజ', 22, 0),
+(21, 'రామ్ చరణ్ తెజ', 22, 0),
+(22, 'చిరంజీవి', 22, 0),
+(23, 'చిరంజీవి', 22, 0),
+(24, 'చిరంజీవి', 22, 0),
+(25, 'చరణ్ తెజ <br>', 22, 0),
+(26, 'పవన్ కళ్యాణ్', 22, 0),
+(27, 'పవన్ కళ్యాణ్', 22, 0);

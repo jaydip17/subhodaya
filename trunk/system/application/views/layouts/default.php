770b0d6e18d042bd4133bb59c6c73d7b395c741c
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-09-21 05:21:34 +0000

    gallery

--- trunk/system/application/views/layouts/default.php
+++ trunk/system/application/views/layouts/default.php
@@ -8,7 +8,7 @@
 <link href="<?=base_url();?>assets/style.css" rel="stylesheet" type="text/css" />
 <!-- for tabs js ans css -->
 <script src="<?=base_url();?>assets/Scripts/swfobject_modified.js" type="text/javascript"></script>
-<script type="text/javascript" src="<?=base_url();?>assets/js/yahoo-dom-event.js"></script>
+
 <script type="text/javascript" src="<?=base_url();?>assets/js/element-min.js"></script>
 <script type="text/javascript" src="<?=base_url();?>assets/js/tabview-min.js"></script>
 <link href="<?=base_url();?>assets/fonts-min.css" rel="stylesheet" type="text/css" />

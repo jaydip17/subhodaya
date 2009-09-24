2c0b4adea5b4f7f176e56217250ee4ed1a8b72dc
vsujatha87 <vsujatha87@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-09-24 07:51:24 +0000

    video_view.php

--- system/application/views/video_view.php
+++ system/application/views/video_view.php
@@ -1,15 +1,15 @@
 <?php      
    foreach($hotvideos->result() as $video){
         ?>
-      <div style="float:left;vertical-align:middle;padding-right:px;padding-bottom:5px;padding-top:2px;">
+      <div style="float:left;vertical-align:middle;padding-right:4px;padding-bottom:2px;padding-top:2px;margin-left:8px;">
       <div style="vertical-align:middle;float:left;text-align:justify;padding-right:10px;">
        <a href="<?=base_url();?>video/index/<?=$video->id?>/<?=$video->video_cat_id?>">
-        <div id="catvideo_time"><?=$video->time?></div> <img id="img" src="<?=base_url();?>assets/videos/image_preview/home_image<?=$video->id?>_thumb.jpg"></a>
+        <div id="catvideo_time"><?=$video->time?></div style="margin-left:25px;"> <img id="img" src="<?=base_url();?>assets/videos/image_preview/home_image<?=$video->id?>_thumb.jpg"></a>
        </div>
-       
-       <span style="font-size:12px;font-weight:bold;"><?=$video->insert_date?></span><br/><span style="font-size:12px;font-weight:bold;"><?=$video->name?></span><br/>
-          <span style="font-size:12px;font-weight:bold;color:#FF6507;padding-top:7px;"><?=$video->no_of_views?> views</span>
-          <br/><span style="font-size:12px;font-weight:bold;padding-top:3px;"><?=$video->time?></span><br/><div id="hotimg"><?=$more['36']->matter?>: <span style="color:#FF6507;"><?=$video->rating?></span></div>
+         
+       <span style="font-size:12px;font-weight:bold;padding-left:8px;"><?=$video->insert_date?></span><br/><span style="font-size:12px;font-weight:bold;padding-left:8px;"><?=$video->name?></span><br/>
+          <span style="font-size:12px;font-weight:bold;color:#FF6507;padding-top:8px;padding-left:8px;"><?=$video->no_of_views?> views</span>
+          <br/><span style="font-size:12px;font-weight:bold;padding-top:3px;padding-left:8px;"><?=$video->time?></span><br/><div id="hotimg"><?=$more['36']->matter?>: <span style="color:#FF6507;"><?=$video->rating?></span></div>
           
            </div>
              

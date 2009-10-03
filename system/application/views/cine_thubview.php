cb5bfe90a6ddecde43c68b477e96ede79e203fca
srivalli.ou <srivalli.ou@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-10-03 14:30:57 +0000

    new mainpage changes

--- system/application/views/cine_thubview.php
+++ system/application/views/cine_thubview.php
@@ -3,8 +3,8 @@
 <? if(!empty($details)){ 
 $count=0; foreach($details as $row):?>
 <div style="height: 105px" id="telugufont1">
-<a href="<?=base_url()?>cinema/inner/<?=$row->id?>"><img src="<?=base_url();?>assets/cinema/ceni_img<?=$row->id ?>_thumb.jpg" id="img" style="float: left;margin: 2px 4px 2px 2px;" ></img></a>
-<font id="mainnews"><a href="<?=base_url()?>cinema/inner/<?=$row->id?>"><? echo $row->heading; ?></a></font><br>
+<a href="<?=base_url()?>cinema/inner/<?=$row->id?>/<?=$row->type?>"><img src="<?=base_url();?>assets/cinema/ceni_img<?=$row->id ?>_thumb.jpg" id="img" style="float: left;margin: 2px 4px 2px 2px;" ></img></a>
+<font id="mainnews"><a href="<?=base_url()?>cinema/inner/<?=$row->id?>/<?=$row->type?>"><? echo $row->heading; ?></a></font><br>
 <? echo $row->summary; ?>
 </div><hr/>
 <?if($count==3){
--- system/application/views/linklistview.php
+++ system/application/views/linklistview.php
@@ -1,7 +1,7 @@
 <div style="height: 470px;">
 <? foreach($details as $row):?>
 <ul id="mainnews">
-<li><a href="<?=base_url()?>news/newsdetails/<?=$row->id?>"><? echo $row->heading; ?></a></li>
+<li style="padding: 3px;"><a href="<?=base_url()?>news/newsdetails/<?=$row->id?>/<?=$row->type?>"><? echo $row->heading; ?></a></li>
 </ul>
 <?   endforeach;  ?></div>
 <div align="right" style="width: 100%;"><? echo $paginate;?></div>

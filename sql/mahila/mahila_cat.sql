2bf3e87610666033ea6482c408fc7166cf8d355a
srivalli.ou <srivalli.ou@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-10-05 11:21:23 +0000

    more,mahila changed

--- sql/mahila/mahila_cat.sql
+++ sql/mahila/mahila_cat.sql
@@ -1,13 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.5
+-- version 3.1.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 16, 2009 at 11:26 AM
--- Server version: 5.0.51
--- PHP Version: 5.2.5
+-- Generation Time: Oct 01, 2009 at 06:38 AM
+-- Server version: 5.1.32
+-- PHP Version: 5.2.9-1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `subhodaya`
@@ -21,10 +27,10 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 DROP TABLE IF EXISTS `mahila_cat`;
 CREATE TABLE IF NOT EXISTS `mahila_cat` (
-  `id` int(10) NOT NULL auto_increment,
-  `cat_name` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `active` int(1) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `cat_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `active` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
@@ -38,5 +44,5 @@ INSERT INTO `mahila_cat` (`id`, `cat_name`, `active`) VALUES
 (4, 'యోగ', 0),
 (5, 'డైట్ & హెల్త్', 0),
 (6, 'మేహెంది', 0),
-(7, 'బ్యూటీ టిప్స్ ', 0),
-(8, 'ఫుడ్', 0);
+(7, 'బ్యుటి టిప్స్', 0),
+(8, 'ఆహారం', 0);

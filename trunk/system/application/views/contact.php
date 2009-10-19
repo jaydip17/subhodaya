cdb5b67ae1bbe857d710f5bc979fdda228f4f5e4
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-10-19 05:00:47 +0000

    validations

--- trunk/system/application/views/contact.php
+++ trunk/system/application/views/contact.php
@@ -7,38 +7,38 @@
 	<tr>
 	    <td style="color:red">*</td>
 		<td>Name</td>
-		<td><input type="text"  name="name" id="name"></td>
-		<td style="color: red;"><?=$this->session->flashdata('name') ?></td>
+		<td><input type="text"  name="name" id="name" value="<?=$this->session->flashdata('name') ?>"></td>
+		<td style="color: red;"><?=$this->session->flashdata('name1') ?></td>
 	</tr>
 	<tr>
 	    <td style="color:red">*</td>
 		<td>Email-id</td>
-		<td><input type="text" name="mailid" id="mailid"></td>
-		<td style="color: red;"><?=$this->session->flashdata('mailid') ?></td>
+		<td><input type="text" name="mailid" id="mailid" value="<?=$this->session->flashdata('mailid') ?>"></td>
+		<td style="color: red;"><?=$this->session->flashdata('mailid1') ?></td>
 	</tr>
 	<tr>
 	    <td style="color:red">*</td>
 		<td>Mobile/Phone</td>
-		<td><input type="text" name="phone" id="phone"></td>
-		<td style="color: red;"><?=$this->session->flashdata('phone') ?></td>
+		<td><input type="text" name="phone" id="phone" value="<?=$this->session->flashdata('phone') ?>"></td>
+		<td style="color: red;"><?=$this->session->flashdata('phone1') ?></td>
 	</tr>
 	<tr>
 	    <td style="color:red"></td>
 		<td>City</td>
-		<td><input type="text" name="city" id="city"></td>
+		<td><input type="text" name="city" id="city" value="<?=$this->session->flashdata('city') ?>"></td>
 		<td></td>
 	</tr>
 	<tr>
 		<td style="color:red"></td>
 		<td>Company/Organization</td>
-		<td><input type="text" name="company" id="company"></td>
+		<td><input type="text" name="company" id="company" value="<?=$this->session->flashdata('comp') ?>"></td>
 		<td></td>
 	</tr>
 	<tr>
 	    <td style="color:red">*</td>
 		<td>Your Message</td>
-		<td><textarea style="height: 100px;width: 150px;" name="message" id="message"></textarea></td>
-		<td style="color: red;"><?=$this->session->flashdata('message')?></td>
+		<td><textarea style="height: 100px;width: 150px;" name="message" id="message" ></textarea></td>
+		<td style="color: red;"><?=$this->session->flashdata('message1')?></td>
 	</tr>
 	<tr >
 		<td colspan='3' align="center"><input type="submit" name="submit" value="submit"></td>

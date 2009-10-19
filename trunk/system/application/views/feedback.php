02686b3834391ddf4a7be837923fc9d3a8c12c7b
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-10-19 10:14:02 +0000

    validations

--- trunk/system/application/views/feedback.php
+++ trunk/system/application/views/feedback.php
@@ -1,4 +1,4 @@
-<form method="post" name="advertise" action="<?=base_url()?>contact/send_contact/" onsubmit="return feedback_validate()">
+<form method="post" name="advertise" action="<?=base_url()?>contact/send_feedback/" onsubmit="return feedback_validate()">
 <table width="100%" border="0" cellspacing="2" cellpadding="0" style="font-size: 13px;border: 1px solid rgb(55, 137, 195);">
     <tr>
         <td colspan="3" align="center">Please Enter all the details marked <span style="color:red">*</span></td>
@@ -6,27 +6,31 @@
 	<tr>
 	    <td style="color:red">*</td>
 		<td>Name</td>
-		<td><input type="text"  name="name" id="name"></td>
+		<td><input type="text"  name="name" id="name" value="<?=$this->session->flashdata('name') ?>"></td>
+		<td style="color: red;"><?=$this->session->flashdata('name1') ?></td>
 	</tr>
 	<tr>
 	    <td style="color:red">*</td>
 		<td>Email-id</td>
-		<td><input type="text" name="mailid" id="mailid"></td>
+		<td><input type="text" name="mailid" id="mailid" value="<?=$this->session->flashdata('mailid') ?>"></td>
+		<td style="color: red;"><?=$this->session->flashdata('mailid1') ?></td>
 	</tr>
 	<tr>
 	    <td style="color:red"></td>
 		<td>Mobile/Phone</td>
-		<td><input type="text" name="phone" id="phone"></td>
+		<td><input type="text" name="phone" id="phone" value="<?=$this->session->flashdata('phone') ?>"></td>
+		<td style="color: red;"><?=$this->session->flashdata('phone1') ?></td>
 	</tr>
 	<tr>
 	    <td style="color:red"></td>
 		<td>City</td>
-		<td><input type="text" name="city" id="city"></td>
+		<td><input type="text" name="city" id="city" value="<?=$this->session->flashdata('city') ?>"></td>
 	</tr>
 	<tr>
 	    <td style="color:red">*</td>
 		<td>Your Message</td>
 		<td><textarea style="height: 100px;width: 300px;" name="message" id="message"></textarea></td>
+		<td style="color: red;"><?=$this->session->flashdata('message1')?></td>
 	</tr>
 	<tr >
 		<td colspan='3' align="center"><input type="submit" name="submit" value="submit"></td>

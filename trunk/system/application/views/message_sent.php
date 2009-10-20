475693ee6ff350a854febc3d39b52493f7335cd3
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-10-20 04:39:33 +0000

    message_sent.php

--- trunk/system/application/views/message_sent.php
+++ trunk/system/application/views/message_sent.php
@@ -1,4 +1,11 @@
-<table align="center" height="95%" width="95%">
-	<tr><td><?=$this->session->flashdata('messagesent')?></td></tr>
-	<tr><td>Add location</td></tr>
+<table align="center" width="95%" style="border: 1px solid #3789C3;">
+<?php $flash_mesg=$this->session->flashdata('messagesent');
+if($flash_mesg!=''){
+?>
+	<tr><td colspan="2" height="30px;"><?=$flash_mesg?></td></tr>
+	<?php }?>
+	<tr>
+		<td align="center"><img src="<?=base_url()?>assets/ads/Home3.jpg"></td>
+		<td align="center"><img src="<?=base_url()?>assets/ads/Home4.jpg"></td>
+	</tr>
 </table>

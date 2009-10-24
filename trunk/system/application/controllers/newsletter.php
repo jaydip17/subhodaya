6ecb6a443d729b1c7fbd12ea44f65e552e87d9fb
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-10-24 12:03:23 +0000

    newsletter.php

--- trunk/system/application/controllers/newsletter.php
+++ trunk/system/application/controllers/newsletter.php
@@ -25,6 +25,7 @@ class Newsletter extends Controller {
 	{
 		
 		$email = $this->input->get_post('email', TRUE);
+		$name=$this->input->get_post('name', TRUE);
 		$validate = $this->Newsletter_model->check_email_address($email);
 		if($validate==TRUE)
 		{
@@ -36,9 +37,8 @@ class Newsletter extends Controller {
 	                     );
 				if($this->db->insert('newsletter', $data))
 				{
-					$message='Thankyou for subscribing with us</br>.Have a great day!!!';
-				
-				
+					
+					
 					//echo "&nbsp;&nbsp;&nbsp;&nbsp;Successful";
 				}
 				else
@@ -46,6 +46,13 @@ class Newsletter extends Controller {
 			}
 			 else 
 			 $message="&nbsp;&nbsp;&nbsp;&nbsp;Email Exists";
+			 $messa='Thankyou for subscribing with us</br>.Have a great day!!!';
+			 $to=$email;
+			 $subject='Subhodaya Newsletter';
+			 $from='dontreply@subhodaya.com';
+			 $from_name=$name;
+			 $this->send_mail($to,$subject,$messa,$from,$from_name);
+				//echo $stuats;exit;
 		}
 		else
 		 $message="&nbsp;&nbsp;&nbsp;&nbsp;Invalid Email";
@@ -55,11 +62,31 @@ class Newsletter extends Controller {
 		
 	}
     function subscribed(){
-		$message = $this->session->flashdata('message');
+		$message= $this->session->flashdata('message');
 		$this->session->flashdata('messagesent');
 		$this->load->view('newsletter_subscribed');
 		
 	}
+	 function send_mail($to,$subject,$messa,$from,$from_name)
+	 {
+	 	$data = array (
+	 					'message' => $messa,
+	 	);
+	 	$this->load->library('email');
+		$this->email->clear();
+		$this->email->from($from, $from_name);
+		$this->email->to($to);
+		//$this->email->cc('another@another-example.com');
+		//$this->email->bcc('them@their-example.com');
+		$html_message  = $this->load->view('email_layout/invitation_friend',$data,TRUE);
+		$this->email->subject($subject);
+		$this->email->message($html_message);
+		
+		$this->email->send();
+		//echo $this->email->print_debugger();
+	
+		return true;
+	 }
 	
 }
 

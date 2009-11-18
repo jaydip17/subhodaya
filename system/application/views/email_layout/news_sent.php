93f4bd536fe98d7f3890b859c3516fdb15f15e87
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-11-18 05:23:28 +0000

    news_sent.php

---
+++ system/application/views/email_layout/news_sent.php
@@ -0,0 +1,45 @@
+<table width="600" cellspacing="0" cellpadding="0" border="0" >
+ <tbody><tr>
+ <td height="9" width="600"></td>
+ </tr>
+ <tr>
+ <td align="left" style="padding: 0px 0px 10px 20px;">
+ 	<font style="font-weight: bold;font-size: 14px;">Thank you for being with us!</font>
+ 	<p style="color:green;font-weight: bold;">To day headlines</p>
+ 	<div style="float: left;width: 50%;">
+	1.<a href="http://www.subhodaya.com/news/newsdetails/482/2"><?=$h1?></a><br>
+	2.<a href="http://www.subhodaya.com/news/newsdetails/490/7"><?=$h2?></a><br>
+	3.<a href="http://www.subhodaya.com/news/newsdetails/491/4"><?=$h3?></a><br>
+	4.<a href="http://www.subhodaya.com/cinema/inner/235/1"><?=$h4?></a><br>
+	5.<a href="http://www.subhodaya.com/news/newsdetails/478/1"><?=$h5?></a><br>
+	</div>
+	<div>
+	6.<a href="http://www.subhodaya.com/news/newsdetails/497/7"><?=$h6?></a><br>
+	7.<a href="http://www.subhodaya.com/news/newsdetails/484/4"><?=$h7?></a><br>
+	8.<a href="http://www.subhodaya.com/news/newsdetails/446/2"><?=$h8?></a><br>
+	9.<a href="http://www.subhodaya.com/news/newsdetails/489/7"><?=$h9?></a><br>
+	10.<a href="http://www.subhodaya.com/news/newsdetails/394/3"><?=$h10?></a><br>
+	</div>
+</td>
+  </tr>
+  <?php if(isset($subhodaya)){?>
+  <tr><td align="left" style="padding: 0px 0px 10px 20px;">Regards,<br/>Subhodaya.com Team!</td></tr>
+  <?php }?>
+ <tr>
+ <td height="9" width="600"></td>
+ </tr>
+ </tbody></table>
+ <div><h6>This message has been forwarded at the request of maillist@subhodaya.com.If you dont like to get mails from subhodaya.com or unsubscribe, <a href="www.subhodaya.com">Click Here</a><br><br>
+
+In case of any assistance, contact support@subhodaya.com<br>
+Copyright © 2009 subhodaya.com. All rights reserved.</h6></div>
+<!-- <table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><a href="http://www.subhodaya.com"><img  border="0px" src="<?=base_url();?>assets/imgs/logo.jpg" alt="subhodaya.com" width="240" height="60" /></a></td>
+  </tr>
+  <tr>
+    <td style="background-color:#D2D7FF;padding: 10px 10px 10px 10px;font-family:Verdana;font-size:12px">
+    <?= nl2br($message) ?>
+    </td>
+  </tr>
+</table> -->

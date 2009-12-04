4296ec8e93e8a756bba862fe89f372e90dfa2107
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-12-04 04:45:30 +0000

    new template home page

---
+++ trunk/system/application/views/home/video_block.php
@@ -0,0 +1,42 @@
+<div style="width:100%;height:148px; border:0px solid #000">
+	<div id="" style="float:left; width:110px;">
+		<div id="" style="width:99px;height:70px;text-align:center;">
+			<a href="#">
+				<img src="<?=base_url();?>assets/home_images/video/video4.jpg" border="0" style="border:1px solid #779AAC">
+			</a>
+			<span>prayanam</span><br/>
+			<span style="color:#FF6407;">513 views</span><br/>
+			<span >reting:</span><span style="color:#FF6407;">4.5</span>
+		</div>
+	</div>
+	<div id="" style="float:left; width:110px;margin-left:4px;">
+		<div id="" style="width:99px;height:70px;text-align:center;">
+			<a href="#">
+				<img src="<?=base_url();?>assets/home_images/video/vido3.jpg" border="0" style="border:1px solid #779AAC">
+			</a>
+			<span>prayanam</span><br/>
+			<span style="color:#FF6407;">513 views</span><br/>
+			<span >reting:</span><span style="color:#FF6407;">4.5</span>
+		</div>
+	</div>
+	<div id="" style="float:left; width:110px;margin-left:4px;">
+		<div id="" style="width:99px;height:70px;text-align:center;">
+			<a href="#">
+				<img src="<?=base_url();?>assets/home_images/video/vidoe1.jpg" border="0" style="border:1px solid #779AAC">
+			</a>
+			<span>prayanam</span><br/>
+			<span style="color:#FF6407;">513 views</span><br/>
+			<span >reting:</span><span style="color:#FF6407;">4.5</span>
+		</div>
+	</div>
+	<div id="" style="float:left; width:110px;margin-left:4px;">
+		<div id="" style="width:99px;height:70px;text-align:center;">
+			<a href="#">
+				<img src="<?=base_url();?>assets/home_images/video/vidoe2.jpg" border="0" style="border:1px solid #779AAC">
+			</a>
+			<span>prayanam</span><br/>
+			<span style="color:#FF6407;">513 views</span><br/>
+			<span >reting:</span><span style="color:#FF6407;">4.5</span>
+		</div>
+	</div>
+</div>

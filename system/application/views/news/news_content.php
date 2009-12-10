26854f19154c0498a637bd67566e1a3782263ec7
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-12-10 09:39:37 +0000

    news content page

---
+++ system/application/views/news/news_content.php
@@ -0,0 +1,69 @@
+<div>
+	<div style="width:641px ;height:77px;float: left;text-align: left;">
+		<div style="padding-left: 10px;">links</div>
+		<div style="padding-left: 10px;height: 20px;"><span style="font-size: 17px;"><?=$active_news[0]->news_cat?></span></div>
+		<div id="hr"><hr style="border: 1px solid #cee2ed;width: 95%" /></div>
+		<div>
+				<div id="thaja_vartha" style="width:395px;">
+							<div><span style="font-size: 18px;"><?=$this->lang->line('thaja_vartha');?></span></div>
+							<a href="#"><img src="<?=base_url()?>assets/news/news_img<?=$thaja_varhta[0]->id?>_thumb.jpg" style="float: left;"/></a>
+							<span id="headings"><a href="#"><?=$thaja_varhta[0]->heading?></a></span><br/>
+                            <span id="summery"><?=$thaja_varhta[0]->summary?></span>
+                            
+				</div>
+				<div id="side_add" style="width:225px;">
+						<div>
+							<ul>
+							<? $count=0; foreach($thaja_varhta as $row):
+          						 					 if($count==0)
+          											{
+	          											$count++;
+	          											continue;
+          											} ?>
+								<li><a href="#"><?=$row->heading?></a></li>
+										<? if($count==7){
+							         					break;
+							          					}else{
+							          						$count++;
+							          					}
+							          					endforeach; 
+							          					if($count==0){
+							          						echo 'No data found';
+							          					}?>
+							</ul>
+							</div>
+				</div>
+		</div>
+	</div>
+	<div id="content_left_add">
+		<img src="<?=base_url();?>assets/home_images/ads/ad1.jpg"/>
+	</div>
+</div>
+<div id="hr_div"><hr id="hrline"></hr></div>
+<div>
+<div style="float: left;width: 718px;">
+	<div style="float: left;width: 50%;height: 35px;">
+		<div style="text-align: left;padding-left: 25px;"><span style="font-size: 17px;"><?=$this->lang->line('marinni_visheshalu');?></span></div>
+	</div>
+	<div style="float: right;width: 50%;height: 35px;"><span>view</span>
+	<img src="<?=base_url();?>assets/imgs/LIN.png" style="cursor: pointer"onclick="loadNews('content','<?=base_url()?>newslist/listview/<?=$active_news[0]->type?>')" />
+    <img style="cursor: pointer; " src="<?=base_url();?>assets/imgs/FUL.png" onclick="loadNews('content','<?=base_url()?>newslist/thumbview/<?=$active_news[0]->type?>')" />
+	</div>
+	<div id="divcontent">
+		<div style="margin: 200px 0px 0px 80px;border: 1px solid silver;height: 41px;width: 216px;">
+			<span style="color: silver;">Loading...............</span><br>
+	 		<img src="<?=base_url();?>assets/imgs/loadingAnimation.gif" align="middle" ></img>
+	 	</div>
+	</div>
+</div>
+<div id="content_left_news">
+	<div id="content_left" ><div style="padding-top: 7px;">Most Read Stories</div></div>
+	<ul style="text-align: left;">
+		<?php foreach ($active_news as $heading):?>
+		<li><a href=""><?=$heading->heading?></a></li>
+		<?php endforeach;?>
+		
+	</ul>
+	<div><img src="<?=base_url();?>assets/home_images/ads/content_add.jpg"/></div>
+</div>
+</div>

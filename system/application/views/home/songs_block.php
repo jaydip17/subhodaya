790bd84063d6551b3feb64fc370e83282bde6ba1
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-12-15 11:57:27 +0000

    reviews_block.php,songs_block.php

---
+++ system/application/views/home/songs_block.php
@@ -0,0 +1,45 @@
+<div id="movie_review">
+							<div id="sub_window_title" >
+								<div id="title_left_curve">
+								</div>
+								<div id="title_right_curve">
+								</div>
+								<div id="special"><?=(isset($heading)) ? $heading : ''?></div>
+							</div>
+							<div id="sub_window_content">
+								
+								<img style="margin:5px 0px 2px 0px; " src="<?=base_url()?>assets/home_images/news/latest.jpg">
+								<div id="movie" style="width:100%" align="left">
+								
+									<ul style="padding-left:25px;" id="sangeetham">
+											<li >
+												<a href="#">Arya 2</a>
+											</li>
+												<li>
+												<a href="#">Ek Niranjan</a>
+											</li>
+												<li>
+												<a href="#">Bendu Apparao</a>
+											</li>
+												<li>
+												<a href="#">Bumper Offer</a>
+											</li>
+												<li >
+												<a href="#">Magadheera</a>
+											</li>
+												<li>
+												<a href="#">Oye</a>
+											</li>
+									</ul>		
+									</div>
+								<div id="news_more">
+									<a href="#" ><?=(isset($more)) ? $more : ''?></a>
+								</div>
+							</div>
+							<div id="sub_window_footer">
+								<div id="footer_left_curve" >
+								</div>
+								<div id="footer_right_curve">
+								</div>
+							</div>
+						</div>

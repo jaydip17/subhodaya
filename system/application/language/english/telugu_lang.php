dea2b4bef0e2b68f696ea0ff6dcb66699c3a4af5
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2009-12-26 04:25:41 +0000

    telugu_lang.php

---
+++ system/application/language/english/telugu_lang.php
@@ -0,0 +1,95 @@
+<?php
+//home page
+$lang['home']								=	"హొమ్";
+$lang['subh_spec'] 			    			= 	"శుభోదయ. కామ్ ప్రత్యేకం"; 
+$lang['bavishyam'] 			    			= 	"భవిష్యవాణి";
+$lang['srungaram'] 			    			= 	"శృంగారం";
+$lang['eenati_vishe'] 			    		= 	"ఈనాటి విశేషాలు";
+$lang['enka_chavandi'] 			    		= 	"ఇంక చదవండి";
+$lang['more'] 			    				= 	"మరిన్ని";
+$lang['select_rasi']						=	"రాశి సెలెక్ట్ చేసి ఆప్షన్ క్లిక్ చేయండి";
+$lang['day']								=	"దినము";
+$lang['week']								=	"వారము";
+$lang['month']								=	"మాసము ";
+$lang['year']								=	"సంవస్తారం";
+//for news
+$lang['news']								=	"వార్తలు";
+$lang['news_ardikam'] 			    		= 	"ఆర్దికం";
+$lang['news_rasriyam'] 			    		= 	"రాష్ట్రీయం";
+$lang['news_jathiyam'] 			    		= 	"జాతీయం";
+$lang['news_antharja'] 			    		= 	"అంతర్జాతీయం";
+$lang['news_kridalu'] 			    		= 	"క్రీడలు";
+$lang['news_setier'] 			    		= 	"సెటైర్";
+$lang['state_news'] 			    		= 	"జిల్లా వార్తలు";
+$lang['thaja_vartha']						=	"తాజా వార్త ";
+$lang['marinni_visheshalu']					=	"మరిన్ని వార్తా విశేషాలు ";
+$lang['sambanditha']						=	"సంబందిత వార్తలు";
+$lang['evikuda_read']						=	"ఇవి కూడా చదవండి ";
+// cinema page
+$lang['cinema']								=	"సినిమా ";
+$lang['reviews'] 			    			= 	"మూవీ రివ్యూస్";
+$lang['sangitham'] 			    			= 	"సంగీతం";
+$lang['cini_vishesm'] 			    		= 	"సిని విశేషాలు";
+$lang['cini_pukarlu'] 			    		= 	"సినీ పుకార్లు";
+$lang['cini_news'] 			    			= 	"సినీ వార్తలు";
+$lang['cini_shedul'] 			    		= 	"మూవీ షెడ్యుల్ ";
+$lang['interviews'] 			    		= 	"ఇంటర్వ్యూలు ";
+$lang['therachatu'] 			    		= 	"తేరచాటు ";
+$lang['other_poll']							=	"అథర్ పొల్ల్స్ ";
+// poll
+$lang['homepoll'] 			    			= 	"మీ అభిప్రాయం";
+$lang['yes'] 			    				= 	"అవును";
+$lang['no'] 			    				= 	"కాదు";
+$lang['yes_no'] 			    			= 	"చెప్పలేం";
+//gallery
+$lang['photo_gallery'] 			    		= 	"ఫోటో గ్యాలరీ";
+$lang['gallery'] 			    			= 	"గ్యాలరీ";
+//videos
+$lang['videos'] 			    			= 	"వీడియోస్";
+$lang['video'] 			    				= 	"వీడియో";
+//greeings
+$lang['greetings'] 			    			= 	"గ్రీటింగ్స్";
+//mahila
+$lang['mahila'] 			    			= 	"మహిళా";
+$lang['mahi_buty'] 			    			= 	"బ్యుటి టిప్స్ ";
+$lang['mahila_spec'] 			    		= 	"ఆడవాళ్ళ ప్రత్యేకం";
+$lang['home_carer'] 			    		= 	"హోం & కరీర్ ";
+$lang['mahila_fashon'] 			    		= 	"ఫ్యాషన్";
+$lang['mahila_yoga'] 			    		= 	"యోగ";
+$lang['mahila_food'] 			    		= 	"ఆహారం";
+$lang['dite_helth'] 			    		= 	"డైట్ & హెల్త్ ";
+$lang['mahila_mehandi'] 			    	= 	"మెహంది ";
+//sahithi
+$lang['sahithi'] 			    			= 	"సాహితి";
+$lang['sahi_festiva'] 			    		= 	"పండుగలు";
+$lang['sahi_kathalu'] 			    		= 	"కథలు";
+$lang['matru_basha'] 			    		= 	"మాతృ భాష";
+$lang['sahi_sametha'] 			    		= 	"సామెతలు";
+$lang['sahi_kavithalu'] 			    	= 	"కవితలు";
+$lang['sahi_kavithm'] 			    		= 	"కవిత్వం";
+$lang['sahi_janapadam'] 			    	= 	"జానపదాలు";
+$lang['sahi_vyasam'] 			    		= 	"వ్యాసం";
+
+
+
+
+//heading for mails sending
+$lang['galley_mail_succ'] 					= 	"మెసేజ్ పంపబడింది ";
+$lang['gretting_mail_succ'] 				= 	"మెసేజ్ పంపబడింది";
+$lang['cinema_mail_succ'] 			    	= 	"మెసేజ్ పంపబడింది ";
+$lang['news_mail_succ'] 			    	= 	"మెసేజ్ పంపబడింది";
+$lang['mahila_mail_succ'] 			    	= 	"మెసేజ్ పంపబడింది";
+$lang['subhodaya'] 			    			= 	"శుభోదయ";
+$lang['com'] 			    				= 	"కామ్";
+$lang['subhodaya_special'] 			    	= 	"శుభోదయ స్పెషల్ ";
+$lang['subhodaya_heading1'] 			    = 	" చంద్రబాబు కి 'ముని' శాపం  ";
+$lang['subhodaya_heading2'] 			    = 	"ఏదీ నిఘా ? ";
+$lang['subhodaya_heading3'] 			    = 	"అవినీతి చెద !";
+$lang['subhodaya_heading4'] 			    = 	"గుడ్డుకు గుడ్‌బై ! ";
+$lang['subhodaya_heading5'] 			    = 	"నగ్నానికి కూతవేటు దూరంలో బాలీ భామలు";
+$lang['subhodaya_heading6'] 			    = 	"క్రికెట్ 'పరుగు' ప్రపంచంలో 'ఒకే ఒక్కడు'";
+$lang['subhodaya_heading7'] 			    = 	"ఇండియాను బుజ్జగించే పనిలో అమెరికా";
+$lang['subhodaya_heading8'] 			    = 	"అన్ని సమస్యలు పరిష్కరిస్తాం : ప్రధాని ";
+$lang['subhodaya_heading9'] 			    = 	"షాక్ ఇస్తున్న ఇలియానా ";
+$lang['subhodaya_heading10'] 			    = 	"పగ్గాలు లేని 'పసిడి'ధర";
+$lang['subhodaya_heading11'] 			    = 	"హబ్బా... అంటే గ్రీన్ టీ : హాన్సిక";

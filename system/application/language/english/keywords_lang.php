96d954890993c16fce90d61ee4a91d1b1ce82e1f
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2010-01-02 06:14:18 +0000

    keywords_lang.php

---
+++ system/application/language/english/keywords_lang.php
@@ -0,0 +1,52 @@
+<?php
+$lang['news_title']			=	"Telugu | Telugu News | AP Politics | Andhra News | Telangana News | Political Leaders in AP | Telugu News Online | ";
+$lang['news_descrip']		=	"No.1 Telugu News and Entertainment Portal. Telugu News, AP Politics, Andhra News, Telangana News, Political Leaders in AP, Telugu News Online, AP Politics On The Net.";
+$lang['newscat1']			=	"State News";
+$lang['newscat2']			=	"National News";
+$lang['newscat3']			=	"International News";
+$lang['newscat4']			=	"Sports News";
+$lang['newscat5']			=	"Financial News";
+$lang['newscat6']			=	"Setair News";
+//cinema cat
+$lang['cine_cat1']			=	"Cine News | Cinema News";
+$lang['cine_cat2']			=	"Cine Gossips";
+$lang['cine_cat3']			=	"Movie Reviews";
+$lang['cine_cat4']			=	"Movie Shedule";
+$lang['cine_cat5']			=	"Interviews";
+$lang['cine_cat6']			=	"Teracharu News";
+//mahila cat
+$lang['mahila_cat1']			=	"Womens Special";
+$lang['mahila_cat2']			=	"Home & Carrier";
+$lang['mahila_cat3']			=	"Fashion | traditional";
+$lang['mahila_cat4']			=	"Yoga";
+$lang['mahila_cat5']			=	"Dite & Helth";
+$lang['mahila_cat6']			=	"Mehndi";
+$lang['mahila_cat7']			=	"Buty tips | Buty Carrier";
+$lang['mahila_cat8']			=	"Food";
+//sahithi cat
+$lang['sahithi_cat1']			=	"Festival";
+$lang['sahithi_cat2']			=	"Stories | Kathalu";
+$lang['sahithi_cat3']			=	"Mother Tounge | Matru Basha";
+$lang['sahithi_cat4']			=	"Proverbs";
+$lang['sahithi_cat5']			=	"Poetry";
+$lang['sahithi_cat6']			=	"";
+$lang['sahithi_cat7']			=	"Folks";
+$lang['sahithi_cat8']			=	"Essays | Essay";
+//greetings cat
+$lang['gree_cat1']				=	"Birthday Greetings";
+$lang['gree_cat2']				=	"Love Greetings";
+$lang['gree_cat3']				=	"Mothers Day Greetings";
+$lang['gree_cat4']				=	"Friendship Greetings | Friendship Day Greetings";
+$lang['gree_cat5']				=	"Gandhi janthi Greetings";
+$lang['gree_cat6']				=	"Marriage Day Greetings";
+$lang['gree_cat7']				=	"Independence Day Greetings | Independence Day Cards";
+$lang['gree_cat8']				=	"Thank You Greetings | Thank You Cards";
+$lang['gree_cat9']				=	"Telugu New Year Greetings | New Year Cards | New Year 2010 ";
+$lang['gree_cat10']				=	"Festival Greetings | Sankranthi Greetings | Pongal Greetings ";
+$lang['gree_cat11']				=	"All The Best Greetings | All The Best Greeting Cards";
+$lang['gree_cat12']				=	"Miss You Greetings | Telugu Miss You Cards";
+$lang['gree_cat13']				=	"Telugu Sorry Greetings | Telugu Sorry Cards | Sorry Telugu Greetings";
+$lang['gree_cat14']				=	"Welcome Greetings | Telugu Welcome Cards";
+$lang['gree_cat15']				=	"Congratulation Greetings | Congratulation Greeting Cards";
+
+?>

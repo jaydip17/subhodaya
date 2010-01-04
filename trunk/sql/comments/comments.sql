69bd3b7e1edced4a1db698ee38ffe0b5ea798783
gowramalli <gowramalli@84b599c2-5bd3-11de-b31f-81d54249e7d9>
2010-01-04 09:52:12 +0000

    comments.sql

--- trunk/sql/comments/comments.sql
+++ trunk/sql/comments/comments.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 09, 2009 at 12:31 PM
+-- Generation Time: Jan 04, 2010 at 09:50 AM
 -- Server version: 5.1.32
 -- PHP Version: 5.2.9-1
 
@@ -25,26 +25,45 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `comments`
 --
 
+DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `newsid` varchar(10) NOT NULL,
+  `news_type` varchar(100) NOT NULL,
   `mailid` varchar(50) NOT NULL,
   `name` varchar(100) NOT NULL,
   `comment` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `insert_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `newsid`, `mailid`, `name`, `comment`, `insert_date`) VALUES
-(2, '55', 'zxzx', 'zczxc', 'csdsd', '0000-00-00 00:00:00'),
-(3, '55', 'gowramalli@gmail.com', 'malleswari', 'asdfsdf', '2009-11-09 13:03:25'),
-(4, '55', 'gowramalli@gmail.com', 'malleswari', 'హాయ్ మల్లీశ్వరి ', '2009-11-09 13:16:12'),
-(5, '55', 'gowramalli@gmail.com', 'malleswari', 'ద్గ్త్ర్ద్గ్ట్ ', '2009-11-09 13:25:35'),
-(6, '55', 'gowramalli@gmail.com', 'bala', 'hi hello hi hello hi hello hi hello hi hello hi hellohi hello hi hellohi hello hi hellohi hellohi hello', '2009-11-09 15:52:55'),
-(7, '0', 'asdasdf', 'sdfasdf', 'asdfsdfsdf', '2009-11-09 16:34:21'),
-(8, '0', 'asdasdf', 'sdfsadf', 'asdsadf', '2009-11-09 16:35:23'),
-(9, '33', 'sdsd', 'fsdf', 'sfsdfsdf', '2009-11-09 16:36:31');
+INSERT INTO `comments` (`id`, `newsid`, `news_type`, `mailid`, `name`, `comment`, `insert_date`) VALUES
+(10, '42', '', '', '', '', '2009-11-10 10:15:00'),
+(2, '55', '', 'zxzx', 'zczxc', 'csdsd', '0000-00-00 00:00:00'),
+(3, '55', '', 'gowramalli@gmail.com', 'malleswari', 'asdfsdf', '2009-11-09 13:03:25'),
+(4, '55', '', 'gowramalli@gmail.com', 'malleswari', 'హాయ్ మల్లీశ్వరి ', '2009-11-09 13:16:12'),
+(5, '55', '', 'gowramalli@gmail.com', 'malleswari', 'ద్గ్త్ర్ద్గ్ట్ ', '2009-11-09 13:25:35'),
+(6, '55', '', 'gowramalli@gmail.com', 'bala', 'hi hello hi hello hi hello hi hello hi hello hi hellohi hello hi hellohi hello hi hellohi hellohi hello', '2009-11-09 15:52:55'),
+(7, '0', '', 'asdasdf', 'sdfasdf', 'asdfsdfsdf', '2009-11-09 16:34:21'),
+(8, '0', '', 'asdasdf', 'sdfsadf', 'asdsadf', '2009-11-09 16:35:23'),
+(9, '33', '', 'sdsd', 'fsdf', 'sfsdfsdf', '2009-11-09 16:36:31'),
+(11, '42', '', '', 'asdfsdf', '', '2009-11-10 10:17:51'),
+(12, '42', '', '', '', '', '2009-11-10 10:18:31'),
+(13, '42', '', '', '', '', '2009-11-10 10:19:57'),
+(14, '42', '', '', 'sdfsdf', '', '2009-11-10 10:20:05'),
+(15, '42', '', '', '', '', '2009-11-10 10:21:17'),
+(16, '42', '', '', 'sdfsdfg', '', '2009-11-10 10:21:25'),
+(17, '42', '', '', 'dfsdfg', '', '2009-11-10 10:22:20'),
+(18, '42', '', '', '', '', '2009-11-10 10:28:37'),
+(19, '42', '', '', 'asdasd', '', '2009-11-10 10:30:37'),
+(20, '42', '', '', '', '', '2009-11-10 10:32:04'),
+(21, '42', '', '', 'sdfasdfsdfsdfasdf', '', '2009-11-10 10:32:19'),
+(22, '42', '', '', '', '', '2009-11-10 10:33:35'),
+(23, '42', '', '', 'dasdfsadfsadf', '', '2009-11-10 10:33:42'),
+(24, '42', '', 'xcvzxc', 'dfsd', '', '2009-11-10 10:48:52'),
+(25, '78', 'news', '', 'dasdasd', 'asdasdasdasd', '2010-01-02 15:42:23'),
+(26, '37', 'cinema', '', 'c.ramachandraiah', 'zdfsdfsdfsf', '2010-01-02 15:45:26');
